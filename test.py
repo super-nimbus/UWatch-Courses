26e9fef9094f538d1bc0af0d391a7aaaf6414d53
Allen <allenlu@live.ca>
2020-01-26 07:13:01 -0500

    almost fully formatted, only building is wrong

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
+import json
+import re
 
 term = '1201'
 subject = 'CS'
@@ -27,21 +29,93 @@ si = iter(headerlist)
 headerlist = [c+next(si, '') for c in si]
 
 headerlist.insert(0, temp)
+headerlist.pop(3)
 
 print(headerlist)
+print(len(headerlist))
 print('--------------------------')
 
 data = extracted [124:]
 
 i = 0
+
+data = data.strip()
 while i < 5:
     data = data.replace('  ', ' ')
     i += 1
 
-data = data.splitlines();
-for row in data:
-    print(row)
+data = data.splitlines()
+
+
+#for row in data:
+#    print(row)
+
+datalist = []
+for rows in data:
+    innerlist = []
+    for cols in rows.split(' '):
+        innerlist.append(cols)
+    datalist.append(innerlist)
+
+#print(datalist)
+
+for rows in datalist: #fixing formating
+
     
+    if rows[1] == 'LEC':
+        rows.pop(-1)
+        rows.pop(5)
+        temp = rows.pop(2)
+        rows[1] = rows[1] + temp
+        rows[2] = rows[2] + ' ' + rows[3]
+
+        count = 0
+        index = count - 1
+        
+        for char in rows[10]:
+            if rows[10][count].isalpha():
+                rows[10] = rows[10][:count] + ' ' + rows[10][count:]
+                temp = rows.pop(-1)
+                temp = temp.split(' ')
+               # print(temp)
+                if len(temp) > 1:
+                    temp1 = temp[0]
+                    temp2 = temp[1]
+                    rows.append(temp1)
+                    rows.append(temp2)
+                break
+            count += 1
+            index += 1
+                
+    else:
+        rows[1] = rows[1] + rows[2]
+        rows [3] = rows[3] + ' ' + rows [4]
+        rows.pop(2)
+
+    count = 0
+
+   # for char in rows[-1]:
+        
+
+    
+    rows[5] = rows[5].replace(u'\xa0', u' ')
+    #rows[5] = rows[5].replace('b', '')
+
+    
+
+
+
+#print(datalist[1])
+#print(datalist[10])
+#print(len(datalist[0]))
+
+master = {}
+
+for rows in datalist:
+    dictionary = dict(zip(headerlist, rows))
+    master[rows[0]] = dictionary
+
+print(master)
     
 
 
